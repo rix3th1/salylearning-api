dd3e0a123a46d91b03f1b5b2e1de82fcdd2b758e
Richard A. Rich <rojasricor@gmail.com>
2024-03-25 07:12:14 -0500

    Adding swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Salylearning API')
+    .setDescription(
+      'Salylearning API for students to learn books in the salylearning software',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
-  console.info({ message: `salylearning-api is running!` });
 }
 bootstrap();
