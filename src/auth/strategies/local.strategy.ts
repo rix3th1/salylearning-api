ea478cfc13cd2b34fc03f3171d0d7065b76f3b1f
Richard A. Rich <rojasricor@gmail.com>
2024-03-26 22:51:08 -0500

    Adding simple auth, missing improving

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -12,9 +12,6 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   async validate(username: string, password: string): Promise<any> {
     try {
       const user = await this.authService.validateUser(username, password);
-      if (!user) {
-        throw new UnauthorizedException();
-      }
       return user;
     } catch (error) {
       console.error(error.message);
