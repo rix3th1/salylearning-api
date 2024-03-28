a7111fc2ba5aba1ec3abfda0bc22a388de259a14
Richard A. Rich <rojasricor@gmail.com>
2024-03-27 21:43:50 -0500

    Adding support to sendgrid

--- process.d.ts
+++ process.d.ts
@@ -4,5 +4,8 @@ declare namespace NodeJS {
     PROD_PROJECT_URL?: string;
     JWTSECRET?: string;
     JWTEXPIRESIN?: string;
+    SENDGRID_API_KEY?: string;
+    EMAIL_SENDER?: string;
+    NAME_SENDER?: string;
   }
 }
