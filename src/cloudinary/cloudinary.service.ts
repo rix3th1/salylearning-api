1e394c51e86cc9f8bddac4b456bfd9624859d72b
Richard A. Rich <rojasricor@gmail.com>
2024-04-02 21:33:23 -0500

    Added cloudinary module

---
+++ src/cloudinary/cloudinary.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import toStream from 'buffer-to-stream';
+import { UploadApiErrorResponse, UploadApiResponse, v2 } from 'cloudinary';
+
+@Injectable()
+export class CloudinaryService {
+  async subirImagen(
+    file: Express.Multer.File,
+  ): Promise<UploadApiResponse | UploadApiErrorResponse> {
+    return new Promise((resolve, reject) => {
+      const upload = v2.uploader.upload_stream((error, result) => {
+        if (error) return reject(error);
+        resolve(result);
+      });
+      toStream(file.buffer).pipe(upload);
+    });
+  }
+}
