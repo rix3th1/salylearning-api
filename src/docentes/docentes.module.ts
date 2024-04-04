89879be078ba657bb3494070da2b79d0e1d1d2f6
Richard A. Rich <rojasricor@gmail.com>
2024-04-04 08:31:04 -0500

    base docentes.module added

---
+++ src/docentes/docentes.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { DocentesController } from './docentes.controller';
+import { DocentesService } from './docentes.service';
+
+@Module({
+  controllers: [DocentesController],
+  providers: [DocentesService],
+  imports: [PrismaModule],
+})
+export class DocentesModule {}
