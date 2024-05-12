a92086b3bc1ee1df2aa9669c0a27a138a6a38636
Richard A. Rich <rojasricor@gmail.com>
2024-05-12 12:16:19 -0500

    chore: Update NestJS configuration for CORS
    This commit updates the NestJS configuration in the `main.ts` file to enable CORS (Cross-Origin Resource Sharing) for the application. By calling the `app.enableCors()` method, the application now allows requests from different origins, improving the accessibility and integration of the API.

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-  });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
