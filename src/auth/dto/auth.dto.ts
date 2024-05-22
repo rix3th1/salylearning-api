0555985bda5c9d7e92eefc189407c37a8d4f1930
Richard A. Rich <rojasricor@gmail.com>
2024-05-21 19:00:00 -0500

    refactor: Update import paths for AuthDto in auth.dto.ts

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,5 +1,5 @@
 import { PickType } from '@nestjs/swagger';
-import { RegistrarseDto } from 'src/registrarse/dto/registrarse.dto';
+import { RegistrarseDto } from '../../registrarse/dto/registrarse.dto';
 
 export class LoginUsuarioDto extends PickType(RegistrarseDto, [
   'username',
