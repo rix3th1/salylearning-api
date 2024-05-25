18d1c74779d804db1c15dea143452d41bde8bd3a
Richard A. Rich <rojasricor@gmail.com>
2024-05-24 22:19:08 -0500

    refactor: Update error message for duplicate grado del usuario
    This commit updates the error message in the GradoUsuarioController to provide a more specific and accurate description when a duplicate grado del usuario is encountered. It removes the reference to the id de grado, as it is not relevant in this context. This change improves the clarity and user experience when handling duplicate grado del usuario scenarios.

--- src/grado-usuario/grado-usuario.controller.ts
+++ src/grado-usuario/grado-usuario.controller.ts
@@ -85,7 +85,7 @@ export class GradoUsuarioController {
       if (error instanceof Prisma.PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
           throw new BadRequestException(
-            'Ya existe un grado del usuario con el mismo usuario o id de grado',
+            'Ya existe un grado del usuario con el mismo usuario',
           );
         } else if (error.code === 'P2003') {
           throw new BadRequestException(
@@ -124,7 +124,7 @@ export class GradoUsuarioController {
       if (error instanceof Prisma.PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
           throw new BadRequestException(
-            'Ya existe un grado del usuario con el mismo usuario o id de grado',
+            'Ya existe un grado del usuario con el mismo usuario',
           );
         } else if (error.code === 'P2003') {
           throw new BadRequestException(
