d192ea936efb632b8ced793ce1b699d48649de88
Richard A. Rich <rojasricor@gmail.com>
2024-05-25 14:21:13 -0500

    refactor: Add avatars to seed script
    This commit adds avatars to the seed script in the src/seed.ts file. It includes avatars for both docentes and estudiantes, with their respective names, descriptions, and roles. This change enhances the seed data by providing a variety of avatars for users to choose from.

--- src/seed.ts
+++ src/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { $Enums, PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
@@ -35,7 +35,99 @@ async function main() {
     ],
   });
 
-  console.info({ grados, generosLiterarios });
+  const avatars = await prisma.avatar.createMany({
+    skipDuplicates: true,
+    data: [
+      // Avatars para docentes
+      {
+        nom_avatar: 'user01.png',
+        descripcion: 'Avatar 1',
+        rol: $Enums.Rol.DOCENTE,
+      },
+      {
+        nom_avatar: 'user02.png',
+        descripcion: 'Avatar 2',
+        rol: $Enums.Rol.DOCENTE,
+      },
+      {
+        nom_avatar: 'user03.png',
+        descripcion: 'Avatar 3',
+        rol: $Enums.Rol.DOCENTE,
+      },
+      {
+        nom_avatar: 'user04.png',
+        descripcion: 'Avatar 4',
+        rol: $Enums.Rol.DOCENTE,
+      },
+      {
+        nom_avatar: 'user05.png',
+        descripcion: 'Avatar 5',
+        rol: $Enums.Rol.DOCENTE,
+      },
+      {
+        nom_avatar: 'user06.png',
+        descripcion: 'Avatar 6',
+        rol: $Enums.Rol.DOCENTE,
+      },
+      // Avatars para estudiantes
+      {
+        nom_avatar: 'astronauta.png',
+        descripcion: 'Avatar 7',
+        rol: $Enums.Rol.ESTUDIANTE,
+      },
+      {
+        nom_avatar: 'cocodrilo_1.png',
+        descripcion: 'Avatar 8',
+        rol: $Enums.Rol.ESTUDIANTE,
+      },
+      {
+        nom_avatar: 'cocodrilo_2.png',
+        descripcion: 'Avatar 9',
+        rol: $Enums.Rol.ESTUDIANTE,
+      },
+      {
+        nom_avatar: 'conejo_1.png',
+        descripcion: 'Avatar 10',
+        rol: $Enums.Rol.ESTUDIANTE,
+      },
+      {
+        nom_avatar: 'conejo_2.png',
+        descripcion: 'Avatar 11',
+        rol: $Enums.Rol.ESTUDIANTE,
+      },
+      {
+        nom_avatar: 'lobo_niño.png',
+        descripcion: 'Avatar 12',
+        rol: $Enums.Rol.ESTUDIANTE,
+      },
+      {
+        nom_avatar: 'manzana.png',
+        descripcion: 'Avatar 13',
+        rol: $Enums.Rol.ESTUDIANTE,
+      },
+      {
+        nom_avatar: 'mazorca.png',
+        descripcion: 'Avatar 14',
+        rol: $Enums.Rol.ESTUDIANTE,
+      },
+      {
+        nom_avatar: 'saly_bruja.png',
+        descripcion: 'Avatar 15',
+        rol: $Enums.Rol.ESTUDIANTE,
+      },
+      {
+        nom_avatar: 'saly_vaquera.png',
+        descripcion: 'Avatar 16',
+        rol: $Enums.Rol.ESTUDIANTE,
+      },
+    ],
+  });
+
+  console.info({
+    grados,
+    generosLiterarios,
+    avatars,
+  });
 }
 
 main()
