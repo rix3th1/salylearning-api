2059bdf8e72c66621645e5a62e62dd30e09475de
Richard A. Rich <rojasricor@gmail.com>
2024-06-10 10:52:25 -0500

    feat: Update NestFactory.create options in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: ['error', 'warn'],
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
